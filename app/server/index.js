4411282ed9613a43ede9197407136848ef2aab6f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-25 09:00:18 -0700

    Removed shared config

--- app/server/index.js
+++ app/server/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
-const config = require('../shared/config');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(config.port);
+app.listen(port);
